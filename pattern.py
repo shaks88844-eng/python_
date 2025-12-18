e21942261c30459e84daeec6b52a583243c74a37
shakshi <shaks88844@gmail.com>
2025-12-18 22:02:25 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,46 @@
+for i in range(1,6):
+    for j in range(1,i+1):
+     print(i,end=" ")
+    print()     
+
+print()
+for i in range(1,6):
+    for j in range(1,7-i):
+     print(i,end=" ")
+    print()     
+
+print()
+
+
+for i in range(1,6):
+    for j in range(5,i,-1):
+       print("#",end=" ")
+    
+
+    for k in range(i):
+     
+     print("@",end=" ")
+    print()                    
+
+print()   
+for i in range(1,7):
+    for j in range(1,i):
+     print(i-j,end=" ")
+    print()       
+
+print()
+for i in range(1,6):
+     for j in range(i,1,-1):
+      print("#",end=" ")
+     print()   
+for i in range(1,6):
+    for j in range(1,6-i):
+     print("#",end=" ")
+    print()
+
+for i in range(1,9):
+   for j in range(1,i+1):
+     print(j*i, end=" ")
+   print() 
+
+
